360834c68cb974c70a87c0a3898697725ea1488d
murat.guner <muratgunr@gmail.com>
2018-04-10 22:13:12 -0400

    Hello World Yazdim.

--- NCAA/workspace.py
+++ NCAA/workspace.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
 	events = pd.read_csv('data/Events_2010.csv')
 	d = events.groupby(['Season', 'EventTeamID', 'EventPlayerID', 'EventType']).agg({'EventType': 'count'})
 	print(d.head())
+	print('Hello World!') #Hadi bakalim bileklere kuvvet!
