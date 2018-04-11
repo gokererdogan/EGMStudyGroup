0cbe56aa8178d713544ff1e9cdc82169e21ca97d
murat.guner <muratgunr@gmail.com>
2018-04-10 22:34:53 -0400

    Tableri degistirdik

--- NCAA/workspace.py
+++ NCAA/workspace.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 if __name__ == "__main__":
-	events = pd.read_csv('data/Events_2010.csv')
-	d = events.groupby(['Season', 'EventTeamID', 'EventPlayerID', 'EventType']).agg({'EventType': 'count'})
-	print(d.head())
-	print('Hello World!') #Hadi bakalim bileklere kuvvet!
+    events = pd.read_csv('data/Events_2010.csv')
+    d = events.groupby(['Season', 'EventTeamID', 'EventPlayerID', 'EventType']).agg({'EventType': 'count'})
+    print(d.head())
+    print('Hello World!')  # Hadi bakalim bileklere kuvvet!
