ec15399aa352e5b059c1bfa1681ab34bc2f9bdd7
Goker Erdogan <gokererdogan@gmail.com>
2018-04-17 23:54:42 -0400

    Add get_daynum function

--- NCAA/workspace.py
+++ NCAA/workspace.py
@@ -1,4 +1,12 @@
+import numpy as np
 import pandas as pd
+
+
+def get_daynum(events, team1, team2):
+	days1 = events[(events.WTeamID == team1) & (events.LTeamID == team2)].DayNum.unique()
+	days2 = events[(events.WTeamID == team2) & (events.LTeamID == team1)].DayNum.unique()
+	return np.concatenate((days1, days2))
+
 
 if __name__ == "__main__":
     events = pd.read_csv('data/Events_2010.csv')
