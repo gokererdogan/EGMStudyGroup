2df20687a141fc8033fff69c3c1930d17447495b
Goker Erdogan <gokererdogan@gmail.com>
2018-04-24 22:41:38 -0400

    Add either_win_lose function

--- NCAA/workspace.py
+++ NCAA/workspace.py
@@ -3,9 +3,15 @@ import pandas as pd
 
 
 def get_daynum(events, team1, team2):
-	days1 = events[(events.WTeamID == team1) & (events.LTeamID == team2)].DayNum.unique()
-	days2 = events[(events.WTeamID == team2) & (events.LTeamID == team1)].DayNum.unique()
-	return np.concatenate((days1, days2))
+    days1 = events[(events.WTeamID == team1) & (events.LTeamID == team2)].DayNum.unique()
+    days2 = events[(events.WTeamID == team2) & (events.LTeamID == team1)].DayNum.unique()
+    return np.concatenate((days1, days2))
+
+
+def either_win_lose(events, team1, team2):
+    days1 = events[(events.WTeamID == team1) & (events.LTeamID == team2)].DayNum.unique()
+    days2 = events[(events.WTeamID == team2) & (events.LTeamID == team1)].DayNum.unique()
+    return len(days1) > 0 and len(days2) > 0
 
 
 if __name__ == "__main__":
