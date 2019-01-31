f85151a832840519b6ba1b615b30921e009cab5b
Emre <emredemirkaya@gmail.com>
2019-01-30 16:06:53 -0800

    Another katamino solver

--- katamino/solver.py
+++ katamino/solver.py
@@ -1,27 +1,39 @@
 import numpy as np
-from numpy import rot90, fliplr, flipud
+from numpy import rot90, fliplr
 
 from itertools import product
 
 
-def compose(op1, op2):
-    def f(m):
-        return op1(op2(m))
-    f.__name__ = op1.__name__ + '_' + op2.__name__
-    return f
+PIECES = {
+    'orange': np.array([[1, 1, 1],
+                        [0, 0, 1],
+                        [0, 0, 1]], dtype=np.int),
+    'purple': np.array([[1, 1, 1],
+                        [0, 1, 1]], dtype=np.int),
+    'yellow': np.array([[1, 1, 1],
+                        [1, 0, 1]], dtype=np.int)
+}
+
+PIECE_FLAGS = {key: item+1 for (key, item) in zip(PIECES, range(len(PIECES)))}
+
+def get_piece_configs(piece):
+    config_list = []
+    for num_rotate in range(3):
+        piece_candidate = rot90(piece, k=num_rotate)
+        if not is_piece_in_list(piece_candidate, config_list):
+            config_list.append(piece_candidate)
+        piece_candidate = fliplr(piece_candidate)
+        if not is_piece_in_list(piece_candidate, config_list):
+            config_list.append(piece_candidate)
+    return config_list
 
 
-def id(m):
-    return m
-
-
-def rot180(m):
-    return rot90(m, k=2)
-
-
-def rot270(m):
-    return rot90(m, k=-1)
-
+def is_piece_in_list(piece, piece_list):
+    is_flag = False
+    for old_piece in piece_list:
+        if equal(old_piece, piece):
+            is_flag = True
+    return is_flag
 
 def equal(p1, p2):
     if p1.shape == p2.shape:
@@ -29,70 +41,48 @@ def equal(p1, p2):
     return False
 
 
-OPS = list(map(compose, *zip(*product([rot90, rot180, rot270], [id, fliplr, flipud]))))
-
-
-PIECES = {
-    1: np.array([[1, 1, 1],
-                 [0, 0, 1],
-                 [0, 0, 1]], dtype=np.int),
-    2: np.array([[1, 1, 1],
-                 [0, 1, 1]], dtype=np.int),
-    3: np.array([[1, 1, 1],
-                 [1, 0, 1]], dtype=np.int)
-}
-
-
-PIECES_TO_OPS = {}
-for p_i, p in PIECES.items():
-    ps = [p]
-    PIECES_TO_OPS[p_i] = [id]
-    for op in OPS:
-        if all([not equal(e, op(p)) for e in ps]):
-            PIECES_TO_OPS[p_i].append(op)
-            ps.append(op(p))
-
-
-def _place(board, piece, i, j):
-    if board[i, j] == 1:
-        return None
-
-    ie = i + piece.shape[0]
-    je = j + piece.shape[1]
-    if ie > board.shape[0] or je > board.shape[1]:
-        return None
-
+def _place(board, piece, position, flag):
     new_board = board.copy()
-    new_board[i:ie, j:je] += piece
+    new_board[position[0]:position[0] + piece.shape[0],
+        position[1]:position[1] + piece.shape[1]] += flag*piece
 
-    if np.any(new_board > 1):
+    old_empty_spaces = np.sum(board == 0)
+    new_empty_spaces = np.sum(new_board == 0)
+
+    if new_empty_spaces != old_empty_spaces - 5:
         return None
-
-    return new_board
+    else:
+        return new_board
 
 
 def _finished(board):
-    return np.all(board == 1)
+    return np.all(board)
 
 
 def _solve(board, allowed_pieces):
-    for p_i in allowed_pieces:
-        p = PIECES[p_i]
-        for op in PIECES_TO_OPS[p_i]:
-            for i in range(board.shape[0]):
-                for j in range(board.shape[1]):
-                    cur_move = [(p_i, op.__name__, (i, j))]
-                    new_board = _place(board, op(p), i, j)
+
+    for piece_color in allowed_pieces:
+        new_piece = PIECES[piece_color]
+        new_piece_flag = PIECE_FLAGS[piece_color]
+        for new_piece_config in get_piece_configs(new_piece):
+            # print(new_piece_config)
+            for i in range(board.shape[0] - new_piece_config.shape[0] + 1):
+                for j in range(board.shape[1] - new_piece_config.shape[1] + 1):
+                    new_board = _place(board, new_piece_config, (i, j), new_piece_flag)
+
                     if new_board is not None:
+                        # print(new_board)
                         if _finished(new_board):
-                            return cur_move
+                            print(new_board)
+                            return new_board
+
 
                         # find solution of subproblem
                         new_allowed_pieces = allowed_pieces.copy()
-                        new_allowed_pieces.remove(p_i)
+                        new_allowed_pieces.remove(piece_color)
                         subsoln = _solve(new_board, new_allowed_pieces)
                         if subsoln is not None:
-                            return cur_move + subsoln
+                            return new_board
     return None
 
 
@@ -105,7 +95,7 @@ def main():
     # print(PIECES_TO_OPS)
 
     board_size = (5, 3)
-    allowed_pieces = [1, 2, 3]
+    allowed_pieces = ['orange', 'purple', 'yellow']
 
     soln = solve(board_size, allowed_pieces)
     print(soln)
